76c8597e5f1fc2fc6b9d75b5bfabf81192253a7f
Shreyas2706 <shreyassingh1216@gmail.com>
2023-11-22 13:36:04 +0530

    no error

---
+++ largerNumber.java
@@ -0,0 +1,18 @@
+public class largerNumber {
+    public static void main (String args []) {
+        int a = 1 ;
+        int b = 3 ;
+        if (a>b) {
+            System.out.println("a is the largest number");
+
+        }
+        else {
+            System.out.println("b is the largest number");
+
+        }
+
+        // code ran successfully ......!!!!!
+    }
+    
+}
+               
