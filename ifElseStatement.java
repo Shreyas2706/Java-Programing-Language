3636ea77f239e580eb42801c685dc4e5e440f3d8
Shreyas2706 <shreyassingh1216@gmail.com>
2023-11-22 13:15:24 +0530

    final code done

--- ifElseStatement.java
+++ ifElseStatement.java
@@ -1,12 +1,25 @@
 public class ifElseStatement {
     public static void main (String args[]) {
         int age = 22 ;
-        if (age>=18) {
+        /*if (age>=18) {
             System.out.println("adult : drive,vote");
 
         }
         else {
             System.out.println("not an adult");
+        }*/
+
+        // code ran succesfully 
+
+        if (age<=13 && age>=18) {
+            System.out.println("Teenager");
         }
+
+        else {
+            System.out.println("not teenager");
+        }
+
+        
     }
+
 }
