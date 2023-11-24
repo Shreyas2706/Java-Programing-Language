5bfad06b7c26aa65d9fcdbbaec4923a17c99b3df
Shreyas2706 <shreyassingh1216@gmail.com>
2023-11-24 17:29:36 +0530

    some upgrades

--- CalculatorSwtich.java
+++ CalculatorSwtich.java
@@ -2,8 +2,14 @@ import java.util.* ;
 public class CalculatorSwtich {
 public static void main (String args []) {
     Scanner sc = new Scanner (System.in) ;
+    System.out.println("Enter your two number");
 float num1 = sc.nextFloat();
 float num2 = sc.nextFloat();
+System.out.println("Press the following button for performing the following operations");
+System.out.println("1 :adition ");
+System.out.println("2 : subtraction");
+System.out.println("3: Multiplication");
+System.out.println("4: Divisom");
 int operator = sc.nextInt();
 switch(operator){
     case 1 : System.out.println(num1 + num2 );
