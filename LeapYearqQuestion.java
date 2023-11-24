e585febb107da71a4b4de93bd7ce1267b9b2615d
Shreyas2706 <shreyassingh1216@gmail.com>
2023-11-24 23:12:39 +0530

    working

--- LeapYearqQuestion.java
+++ LeapYearqQuestion.java
@@ -1,6 +1,13 @@
 import java.util.* ;
 public class LeapYearqQuestion {
    public static void main (String args []) {
-   
+   Scanner sc = new Scanner(System.in) ;
+   System.out.println("Enter Year");
+   int year = sc.nextInt() ;
+   if (year%4==0 && (year%100==0 || year%400==0)) {
+    System.out.println("Leap Year");
+   } else {
+    System.err.println("Not a leap year");
+   }
    } 
 }
