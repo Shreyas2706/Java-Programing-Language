ca060ef4956e6833ae002e1377a2d284dc20d0ce
Shreyas2706 <shreyassingh1216@gmail.com>
2023-11-24 17:41:23 +0530

    question done

---
+++ PositiveNegetiveQuestion.java
@@ -0,0 +1,15 @@
+import java.util.* ;
+public class PositiveNegetiveQuestion {
+   public static void main (String args []) {
+    Scanner sc = new Scanner (System.in) ;
+    int num1 = sc.nextInt() ;
+        if (num1 > 0) {
+        System.out.println("Positive Number");
+    } else {
+        System.out.println("Negitive Number");
+
+    }
+}
+}
+
+//// done !!!! ......
