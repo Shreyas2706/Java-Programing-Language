c2497a9483b5435745483e6c78a8374d958e096d
Shreyas2706 <shreyassingh1216@gmail.com>
2024-02-25 15:45:37 +0530

    DONE USING BOTH LOOP

---
+++ Print4LineStarPattern.java
@@ -0,0 +1,17 @@
+public class Print4LineStarPattern {
+    public static void main (String args []) {
+        // USING FOR LOOP
+        //for (int i =1 ; i<=4 ; i++) {
+         //   System.out.println("****");
+     // }
+        // CODE DONE SUUCCESSFULLY 
+
+        // NOW USING WHILE LOOP 
+int i = 1 ;
+while (i<=4) {
+    System.out.println("****");
+    i ++ ; 
+}
+    }
+    
+}
