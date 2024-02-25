a62df18719ea9ef38031b61c2a9f15c60bc4c4cd
Shreyas2706 <shreyassingh1216@gmail.com>
2024-02-25 14:30:27 +0530

    success in first run

---
+++ PrintHelloWorld100X.java
@@ -0,0 +1,11 @@
+public class PrintHelloWorld100X {
+    public static void main (String args []) {
+int counter = 0 ;
+while (counter<100) {
+    System.out.println("Hello World") ;
+    counter ++ ;
+}
+System.out.println("Printed Hello World 100X") ;
+    }
+    
+}
