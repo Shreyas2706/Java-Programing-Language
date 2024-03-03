963b72362a38ac7c24f77dd9c32b1fc3f546e5f8
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-03 17:27:02 +0530

    question done through for loop

---
+++ BreakQuestion.java
@@ -0,0 +1,19 @@
+import java.util.* ;
+public class BreakQuestion {
+    public static void main (String args []) {
+        Scanner sc = new Scanner(System.in) ;
+      //  for (int n = sc.nextInt() ; n>0 ; n++){
+            
+         //   if(n%10==0) {
+//break ;
+       //     } 
+       //     System.out.println(n);
+            
+        
+        
+
+  //  }
+   // System.out.println("User intered a multiple of 10");
+    
+}
+}
