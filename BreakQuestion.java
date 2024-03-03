32d10417acc7ea0cecf9801b3b5707bde4874c63
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-03 17:36:50 +0530

    question solved in one go

--- BreakQuestion.java
+++ BreakQuestion.java
@@ -2,18 +2,19 @@ import java.util.* ;
 public class BreakQuestion {
     public static void main (String args []) {
         Scanner sc = new Scanner(System.in) ;
-      //  for (int n = sc.nextInt() ; n>0 ; n++){
+        for (int n = sc.nextInt() ; n>0 ; n++){
             
-         //   if(n%10==0) {
-//break ;
-       //     } 
-       //     System.out.println(n);
+            if(n%10==0) {
+break ;
+            } 
+            System.out.println(n);
             
         
         
 
-  //  }
-   // System.out.println("User intered a multiple of 10");
+    }
+    System.out.println("User entered multiple of 10");
+    
     
 }
 }
