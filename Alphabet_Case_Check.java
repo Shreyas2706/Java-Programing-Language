8941db029aba8e4454103325d2c4e12d647b53ff
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-14 19:50:18 +0530

    New concept done

--- Alphabet_Case_Check.java
+++ Alphabet_Case_Check.java
@@ -5,6 +5,23 @@ public class Alphabet_Case_Check {
         // char ch = name.charAt(2) ;
         // System.out.println(ch);
 
+        Scanner sc = new Scanner(System.in) ;
+        char ch = sc.next().trim().charAt(0) ;
+
+        if (ch>='A' && ch<='Z') {
+            System.out.println("1");
+
+        }  
+
+        else if (ch>='a'&& ch<='z') {
+            System.out.println("0");
+
+        }
+        else {
+            System.out.println("-1");
+        }
+
+
     }
     
 }
