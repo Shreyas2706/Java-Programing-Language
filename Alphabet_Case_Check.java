c0f42acf5a54c2b3c1703e0b591f452f2f13127e
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-14 18:01:44 +0530

    cont configure

---
+++ Alphabet_Case_Check.java
@@ -0,0 +1,10 @@
+import java.util.* ;
+public class Alphabet_Case_Check {
+    public static void main(String[] args) {
+        // String name = "Hello" ;
+        // char ch = name.charAt(2) ;
+        // System.out.println(ch);
+
+    }
+    
+}
