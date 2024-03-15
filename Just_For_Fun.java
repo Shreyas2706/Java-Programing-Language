2874e36a2dde1ecc51d73b8ba3b8fc17a0083642
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-15 19:49:47 +0530

    calculator with loop

---
+++ Just_For_Fun.java
@@ -0,0 +1,13 @@
+public class Just_For_Fun {
+    public static void main(String[] args) {
+        int n = 23597 ;
+        int count = 1 ;
+        while (n>0) {
+            int lastdigit = n%10 ;
+            System.out.print(lastdigit);
+            n /=10 ;
+count ++ ;
+     }
+     System.out.println( ); 
+    }
+}
