2dd039dd98d688e0e58bbe1e70949517d36b6c84
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-15 20:31:58 +0530

    switch concept

---
+++ Switch_Concept.java
@@ -0,0 +1,24 @@
+import java.util.* ;
+public class Switch_Concept {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in) ;
+        String fruit = sc.next() ;
+        switch (fruit) {
+            case "mango":
+                System.out.println("King of all fruit");
+                break;
+        case "apple" :
+        System.out.println("a red fruit");
+        break ;
+        case "orange" :
+        System.out.println("a round apple");
+        break ;
+            default:
+System.out.println("Enter a valid fruit");             
+
+        } {
+
+        }
+    }
+}
