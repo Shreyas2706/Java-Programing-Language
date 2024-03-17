c3ac9377d72f325c9a6569b7c21ede57a397a271
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-17 12:16:54 +0530

    Swap code

---
+++ Swap_Function.java
@@ -0,0 +1,16 @@
+public class Swap_Function {
+    public static void main(String[] args) {
+        // SWAP Exchange value
+        int a = 10 ;
+        int b = 5 ;
+
+        // swap 
+        int temp = a ;
+        a = b ;    
+        b = temp ;
+
+        System.out.println("a = "+a);
+        System.out.println("b = " +b);
+        
+    }
+}
