c37bb746252f9819a3c50dafd137cdfa64b085dd
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-17 13:17:55 +0530

    Function Prpduct

---
+++ Function_Sum.java
@@ -0,0 +1,14 @@
+public class Function_Sum {
+    public static int product(int num1,int num2) {
+        int product = num1*num2 ;
+        return product ;
+
+    }
+    public static void main(String[] args) {
+        int a = 3 ;
+        int b = 5 ;
+        int product = product(a, b);
+        System.out.println("Product = "+product);
+    }
+    
+}
