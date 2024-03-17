abb2036d847517be60fce95277f56470156e296c
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-17 12:39:13 +0530

    Swap concept

--- Just_For_Fun.java
+++ Just_For_Fun.java
@@ -1,16 +1,19 @@
 import java.util.* ;
 public class Just_For_Fun {
-public static int calculate(int num1,int num2) {
-    int sum = num1+num2 ;
-    return sum ;
+public static void main(String[] args) {
+    int a = 100 ;
+    int b = 20 ;
+     
+    // Swap 
+    int temp = a ;
+    a = b ;
+    b = temp ;
+    System.out.println("a = "+a);
+    System.out.println("b = "+b);
+    
+}
 
 }
-public static void main (String args[]) {
-    Scanner sc = new Scanner (System.in) ;
-    int a = sc.nextInt() ;
-    int b = sc.nextInt() ;
-    int sum = calculate(a,b) ;
-    System.out.println("The sum of number is = " +sum );
-}
- }
+
+ 
 
