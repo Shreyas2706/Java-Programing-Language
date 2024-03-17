24987eb2bde99ec7814638b0389c3ef30139ed39
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-17 15:23:34 +0530

    function factorial

---
+++ Function_Loop.java
@@ -0,0 +1,16 @@
+import java.util.* ;
+public class Function_Loop {
+    public static int factorial(int a) {
+        int f = 1 ;
+        for (int i = 1 ; i<=a ; i++) {
+            f = f*i ;
+
+        }
+return f ;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(factorial(4));
+    }
+    
+}
