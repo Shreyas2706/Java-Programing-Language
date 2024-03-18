bb82c31807e8bef7ddc2a2f316362634919bbd58
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-18 19:42:35 +0530

    Factorial practice

--- Just_For_Fun.java
+++ Just_For_Fun.java
@@ -1,16 +1,31 @@
 import java.util.* ;
-public class Just_For_Fun {
-public static void main(String[] args) {
-    Scanner sc = new Scanner (System.in) ;
-int n = sc.nextInt() ;
-int fact = 1 ;
-for (int i = 1 ; i<=n ;i++ ) {
-    fact = i*fact  ;
-}
-System.out.println(fact);
+ public class Just_For_Fun {
+// public static int Factorial(int a) {
+//     int f = 1 ;
+//     for (int i = 1 ; i<=a ; i++) {
+//         f = f*i ;
+//     }
+//     return f ;
 
-    
-}
+// }
+// public static void main(String[] args) {
+//     System.out.println(Factorial(4));
+// }
+
+    // PRODUCT OF 2 SUM 
+
+    public static int product(int a , int b ) {
+        
+    }
+
+
+
+
+
+
+
+
+
 
 }
 
