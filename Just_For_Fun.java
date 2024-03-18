a0d6c895e574494d826f3b3ead41c6ddbaedaa57
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-18 19:49:38 +0530

    product question practice

--- Just_For_Fun.java
+++ Just_For_Fun.java
@@ -14,8 +14,17 @@ import java.util.* ;
 
     // PRODUCT OF 2 SUM 
 
-    public static int product(int a , int b ) {
-        
+    public static int product(int num1 , int num2 ) {
+         int product = num1*num2;
+    return product ; 
+    } 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // int product = product(a, b) ;
+        // System.out.println(product);
+        System.out.println(product(2, 3));
     }
 
 
