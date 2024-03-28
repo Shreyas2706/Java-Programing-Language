f045ff2dd35ce419380970ae7a7a4692b5613c92
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-28 20:40:25 +0530

    New concept binomial cofficient

--- Binomial_Cofficient.java
+++ Binomial_Cofficient.java
@@ -1,3 +1,27 @@
 public class Binomial_Cofficient {
+public static int factorial(int num1) {
+    int f = 1 ;
+    for(int i = 1 ; i<=num1 ; i++) {
+    f = f * i ;
+    }
+    return f ;
+}
+
+public static int binocoffic(int a , int b) {
+    int n = factorial(a) ;
+    int r = factorial(b);
+    int diff = factorial(a - b) ;
+    int bino = n / (r* diff) ;
+    return bino ;
+}
+
+public static void main (String args []) {
+    System.out.println(binocoffic(5, 2));
+}
+
+    
+    
+        
+
     
 }
