287ace3b77de1db2af208d155bb2ba418dfec98f
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-28 19:23:35 +0530

    Factorial revision

--- Just_For_Fun.java
+++ Just_For_Fun.java
@@ -1,42 +1,19 @@
 import java.util.* ;
  public class Just_For_Fun {
-// public static int Factorial(int a) {
-//     int f = 1 ;
-//     for (int i = 1 ; i<=a ; i++) {
-//         f = f*i ;
-//     }
-//     return f ;
-
-// }
-// public static void main(String[] args) {
-//     System.out.println(Factorial(4));
-// }
-
-    // PRODUCT OF 2 SUM 
-
-    public static int product(int num1 , int num2 ) {
-         int product = num1*num2;
-    return product ; 
-    } 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in) ;
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        // int product = product(a, b) ;
-        // System.out.println(product);
-        System.out.println(product(2, 3));
+public static int factorial(int a ) {
+    int f = 1 ;
+    for(int i = 1 ; i<=a ; i++) {
+         f = f*i ;
     }
-
-
-
-
-
-
-
-
-
+    return f ;
+}   
+public static void main (String args []) {
+    System.out.println(factorial(4));
+}
 
 }
+
+
 
  
 
