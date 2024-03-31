40c0e661f448d6f93b89e50f21a13b040c4ed408
Shreyas2706 <shreyassingh1216@gmail.com>
2024-03-31 23:50:35 +0530

    prime number revison

--- Just_For_Fun.java
+++ Just_For_Fun.java
@@ -1,27 +1,32 @@
 import java.util.* ;
  public class Just_For_Fun {
-    public static void main (String args []) {
-Scanner sc = new Scanner (System.in) ;
-int a = sc.nextInt() ;
-boolean isprime = true ;
-for (int i = 2 ; i<=a-1 ; i++) {
-    if (a%i==0) {
-        isprime = false ;
-    }
-}
-if (isprime = true) {
-    System.out.println("not prime");
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in) ;
+    int a = sc.nextInt() ;
+    boolean isprime = true ;
+    for(int i = 2 ; i<=a-1 ; i++) {
+        if (a%i==0) {
+            isprime = false ;
+        }
+        }
+        if(isprime==false) {
+            System.out.println("not a prime number");
 
-}
-else{
-    System.out.println("Prime");
-}
+        }
+        else{
+System.out.println("prime number");
+        }
     
 
+    
+
+    
+}
+
 
 }
 
- }
+ 
 
  
 
